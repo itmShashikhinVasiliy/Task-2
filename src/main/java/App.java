4a2173a9e410c3cc452324c8ffddd8c67f21c8b3
wvterry <ez9616371599@yandex.ru>
2023-12-14 21:16:53 +0300

    add code

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,21 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+        Cat bean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean2.getName());
+
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean3.getName());
+
+
+        System.out.println(bean == bean1);
+        System.out.println(bean2 == bean3);
     }
 }
--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,6 +1,10 @@
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
+
+
+
 
 @Configuration
 public class AppConfig {
@@ -10,5 +14,14 @@ public class AppConfig {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
+    }
+
+
+    @Scope(value = "prototype")
+    @Bean(name = "cat")
+    public Cat getcat(){
+        Cat kit = new Cat();
+        kit.setName("Kit");
+        return kit;
     }
 }
